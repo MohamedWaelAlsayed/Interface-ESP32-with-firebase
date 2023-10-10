e677977b65a6f3b59ac2d222060395e78387cb43
Mohamed Wael Alsayed <mohamed.wael.elsuid@gmail.com>
2023-10-10 08:11:47 +0300

    Indent entire file using vim

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -76,99 +76,99 @@ unsigned long timerDelay = 30000;
 
 // Initialize WiFi
 void initWiFi() {
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.println("Connecting to WiFi ..");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print('.');
-    delay(1000);
-  }
-  Serial.print("\nConnected Successfully\nIp is: ");
-  Serial.println(WiFi.localIP());
-  Serial.println();
+	WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+	Serial.println("Connecting to WiFi ..");
+	while (WiFi.status() != WL_CONNECTED) {
+		Serial.print('.');
+		delay(1000);
+	}
+	Serial.print("\nConnected Successfully\nIp is: ");
+	Serial.println(WiFi.localIP());
+	Serial.println();
 }
 
 // Function that gets current epoch time
 unsigned long getTime() {
-  time_t now;
-  struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)) {
-    // Serial.println("Failed to obtain time");
-    return(0);
-  }
-  time(&now);
-  return now;
+	time_t now;
+	struct tm timeinfo;
+	if (!getLocalTime(&timeinfo)) {
+		// Serial.println("Failed to obtain time");
+		return(0);
+	}
+	time(&now);
+	return now;
 }
 
 void setup(){
-  Serial.begin(115200);
+	Serial.begin(115200);
 
- // Start the DS18B20 sensor
-  sensors.begin();
+	// Start the DS18B20 sensor
+	sensors.begin();
 
-//start Wifi
-  initWiFi();
+	//start Wifi
+	initWiFi();
 
-//Configure times   
-  configTime(0, 0, ntpServer);
+	//Configure times   
+	configTime(0, 0, ntpServer);
 
-  // Assign the api key (required)
-  config.api_key = API_KEY;
+	// Assign the api key (required)
+	config.api_key = API_KEY;
 
-  // Assign the user sign in credentials
-  auth.user.email = USER_EMAIL;
-  auth.user.password = USER_PASSWORD;
+	// Assign the user sign in credentials
+	auth.user.email = USER_EMAIL;
+	auth.user.password = USER_PASSWORD;
 
-  // Assign the RTDB URL (required)
-  config.database_url = DATABASE_URL;
+	// Assign the RTDB URL (required)
+	config.database_url = DATABASE_URL;
 
-  Firebase.reconnectWiFi(true);
-  fbdo.setResponseSize(4096);
+	Firebase.reconnectWiFi(true);
+	fbdo.setResponseSize(4096);
 
-  // Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+	// Assign the callback function for the long running token generation task */
+	config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
 
-  // Assign the maximum retry of token generation
-  config.max_token_generation_retry = 5;
+	// Assign the maximum retry of token generation
+	config.max_token_generation_retry = 5;
 
-  // Initialize the library with the Firebase authen and config
-  Firebase.begin(&config, &auth);
+	// Initialize the library with the Firebase authen and config
+	Firebase.begin(&config, &auth);
 
-  // Getting the user UID might take a few seconds
-  Serial.println("Getting User UID");
-  while ((auth.token.uid) == "") {
-    Serial.print('.');
-    delay(1000);
-  }
-  // Print user UID
-  uid = auth.token.uid.c_str();
-  Serial.print("User UID: ");
-  Serial.println(uid);
+	// Getting the user UID might take a few seconds
+	Serial.println("Getting User UID");
+	while ((auth.token.uid) == "") {
+		Serial.print('.');
+		delay(1000);
+	}
+	// Print user UID
+	uid = auth.token.uid.c_str();
+	Serial.print("User UID: ");
+	Serial.println(uid);
 
-  // Update database path
-  databasePath = "/UsersData/" + uid + "/readings";
+	// Update database path
+	databasePath = "/UsersData/" + uid + "/readings";
 }
 
 void loop(){
 
-  // Send new readings to database
-  if (Firebase.ready() && (millis() - sendDataPrevMillis > timerDelay || sendDataPrevMillis == 0)){
-    sendDataPrevMillis = millis();
+	// Send new readings to database
+	if (Firebase.ready() && (millis() - sendDataPrevMillis > timerDelay || sendDataPrevMillis == 0)){
+		sendDataPrevMillis = millis();
 
-    //Get current timestamp
-    timestamp = getTime();
-    Serial.print ("time: ");
-    Serial.println (timestamp);
+		//Get current timestamp
+		timestamp = getTime();
+		Serial.print ("time: ");
+		Serial.println (timestamp);
 
-    parentPath= databasePath + "/" + String(timestamp);
+		parentPath= databasePath + "/" + String(timestamp);
 
-    humidity = analogRead(sensorPin);
-    humidity = (humidity/4095)*100;
-    temperature = sensors.getTempCByIndex(0);
+		humidity = analogRead(sensorPin);
+		humidity = (humidity/4095)*100;
+		temperature = sensors.getTempCByIndex(0);
 
-    json.set(tempPath.c_str(), String(temperature));
-    json.set(humPath.c_str(), String(humidity));
-    json.set(timePath, String(timestamp));
-    Serial.printf("Set json... %s\n", Firebase.RTDB.setJSON(&fbdo, parentPath.c_str(), &json) ? "ok" : fbdo.errorReason().c_str());
-    
-  }
+		json.set(tempPath.c_str(), String(temperature));
+		json.set(humPath.c_str(), String(humidity));
+		json.set(timePath, String(timestamp));
+		Serial.printf("Set json... %s\n", Firebase.RTDB.setJSON(&fbdo, parentPath.c_str(), &json) ? "ok" : fbdo.errorReason().c_str());
+
+	}
 }
